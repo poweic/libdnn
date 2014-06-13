39b78c2a79c8aff5a03e0e55f6ba71eab3430d34
Boton <botonchou@gmail.com>
2014-06-14 01:31:55 +0800

    Minor refinement on example codes.

--- example/example1.sh
+++ example/example1.sh
@@ -6,8 +6,8 @@ TEST=data/a1a.t
 stacked_rbm=model/a1a.rbm
 model=model/a1a.model
 
-opts="--input-dim 119"
+opts="--input-dim 123"
 
-dnn-init $opts --output-dim 2 --nodes 256-256 $TRAIN $stacked_rbm
-dnn-train $opts $TRAIN $stacked_rbm $model --min-acc 0.8
-dnn-predict $opts $TEST $model
+../dnn-init $opts --output-dim 2 --nodes 256-256 $TRAIN $stacked_rbm
+../dnn-train $opts $TRAIN $stacked_rbm $model --min-acc 0.8
+../dnn-predict $opts $TEST $model
--- example/example2.sh
+++ example/example2.sh
@@ -8,6 +8,6 @@ model=model/train.dat.model
 
 opts="--normalize 1 --input-dim 20"
 
-dnn-init $opts --output-dim 2 --nodes 64-64 $TRAIN $stacked_rbm
-dnn-train $opts $TRAIN $stacked_rbm $model --min-acc 0.74
-dnn-predict $opts $TEST $model
+../dnn-init $opts --output-dim 2 --nodes 64-64 $TRAIN $stacked_rbm
+../dnn-train $opts $TRAIN $stacked_rbm $model --min-acc 0.74
+../dnn-predict $opts $TEST $model
