6a6250638230446264c1e366a67a78939ecd017b
Boton Chou <botonchou@gmail.com>
2015-01-03 01:54:29 +0800

    [Bug Fixed] Forgot to init sum to 0 in downsample.
    Speed up cuda kernels by refactoring.
    Change some input arguments to const.

--- src/cnn-utility.cu
+++ src/cnn-utility.cu
@@ -28,9 +28,9 @@
   const int nThreads = blockDim.x * blockDim.y;\
   const int tx = threadIdx.x;\
   const int ty = threadIdx.y;\
-  const int tid = tx + blockDim.x * ty;\
-  const int x0 = blockIdx.x * blockDim.x;\
-  const int y0 = blockIdx.y * blockDim.y;\
+  int tid = tx + blockDim.x * ty;\
+  int x0 = blockIdx.x * blockDim.x;\
+  int y0 = blockIdx.y * blockDim.y;\
   const int x = x0 + tx;\
   const int y = y0 + ty;
 
@@ -121,16 +121,9 @@ __device__ void load_kernel_into_shm(float* const K, const float* const kernel,
     int kH, int kW, int tid, int nThreads) {
 
   // Copy kernel in global memory to shared memory
-  int nTotal = kW * kH;
-  int avgToLoad = (kW * kH) / nThreads + 1;
-
-  for (int i=0; i<avgToLoad; ++i) {
-    int id = tid + i * nThreads;
-
-    if (id >= nTotal) break;
-
-    int xx = id / kH,
-	yy = id % kH;
+  for (; tid<kW * kH; tid += nThreads) {
+    int xx = tid / kH,
+	yy = tid % kH;
 
     if (xx >= kW || yy >= kH) continue;
 
@@ -160,30 +153,28 @@ __global__ void convn_valid_kernel_with_shm2(float *output, const float *data,
       HEIGHT_STEP = blockDim.y + kH - 1;
 
   int nTotal = WIDTH_STEP * HEIGHT_STEP;
-  int avgToLoad  = nTotal / nThreads + 1;
 
-  for (int i=0; i<avgToLoad; ++i) {
-    int id = tid + i * nThreads;
-
-    if (id >= nTotal) break;
-
-    int xx = id / HEIGHT_STEP + x0,
-	yy = id % HEIGHT_STEP + y0;
+  for (; tid<nTotal; tid += nThreads) {
+    int xx = tid / HEIGHT_STEP + x0,
+	yy = tid % HEIGHT_STEP + y0;
 
     if (xx >= W || yy >= H) continue;
 
     // rotate data 180 degree
-    D[id] = data[ (W - 1 - xx) * H + (H - 1 - yy) ];
+    D[tid] = data[ (W - 1 - xx) * H + (H - 1 - yy) ];
   }
   __syncthreads();
 
+  if (x >= vW || y >= vH)
+    return;
+
   float sum = 0;
+  D += tx * HEIGHT_STEP + ty;
   for (int i = 0; i < kW; ++i)
     for(int j = 0; j < kH; ++j)
-      sum += K[ i * kH + j ] * D[ (tx + i) * HEIGHT_STEP + (ty + j) ]; 
+      sum += K[ i * kH + j ] * D[ i * HEIGHT_STEP + j ]; 
 
-  if (x < vW && y < vH)
-    output[ x * vH + y ] += sum;
+  output[ x * vH + y ] += sum;
 } 
 
 __global__ void convn_valid_kernel_with_shm(float *output, const float *data,
@@ -208,40 +199,35 @@ __global__ void convn_valid_kernel_with_shm(float *output, const float *data,
       HEIGHT_STEP = blockDim.y + kH - 1;
 
   int nTotal = WIDTH_STEP * HEIGHT_STEP;
-  int avgToLoad  = nTotal / nThreads + 1;
 
-  for (int i=0; i<avgToLoad; ++i) {
-    int id = tid + i * nThreads;
-
-    if (id >= nTotal) break;
-
-    int xx = id / HEIGHT_STEP + x0,
-	yy = id % HEIGHT_STEP + y0;
+  for (; tid<nTotal; tid += nThreads) {
+    int xx = x0 + tid / HEIGHT_STEP,
+	yy = y0 + tid % HEIGHT_STEP;
 
     if (xx >= W || yy >= H) continue;
 
-    D[id] = data[ xx * H + yy ];
+    D[tid] = data[ xx * H + yy ];
   }
   __syncthreads();
 
+  if (x >= vW || y >= vH)
+    return;
+
+  D += tx * HEIGHT_STEP + ty;
   float sum = 0;
   for (int i = 0; i < kW; ++i)
     for(int j = 0; j < kH; ++j)
-      sum += K[ i * kH + j ] * D[ (tx + i) * HEIGHT_STEP + (ty + j) ]; 
+      sum += K[ i * kH + j ] * D[ i * HEIGHT_STEP + j ]; 
 
-  if (x < vW && y < vH)
-    output[ x * vH + y ] = sum;
+  output[ x * vH + y ] = sum;
 } 
 
 __global__ void convn_valid_kernel(float *output, float *data, float *kernel,
-    int H, int W, int kH, int kW) { 
-
-  int tx = threadIdx.x;
-  int ty = threadIdx.y;
+    const int H, const int W, const int kH, const int kW) { 
 
   // Matrix index
-  int x = blockIdx.x*blockDim.x + tx;
-  int y = blockIdx.y*blockDim.y + ty;
+  int x = blockIdx.x*blockDim.x + threadIdx.x;
+  int y = blockIdx.y*blockDim.y + threadIdx.y;
 
   // vH, vW stands for valid H and valid W
   const int vH = H - kH + 1,
@@ -265,14 +251,11 @@ __global__ void convn_valid_kernel(float *output, float *data, float *kernel,
 } 
 
 __global__ void convn_same_kernel(float *output, float *data, float *kernel,
-    int H, int W, int kH, int kW) { 
-
-  int tx = threadIdx.x;
-  int ty = threadIdx.y;
+    const int H, const int W, const int kH, const int kW) { 
 
   // Matrix index
-  int x = blockIdx.x*blockDim.x + tx;
-  int y = blockIdx.y*blockDim.y + ty;
+  const int x = blockIdx.x*blockDim.x + threadIdx.x;
+  const int y = blockIdx.y*blockDim.y + threadIdx.y;
 
   if (x >= W || y >= H)
     return;
@@ -295,7 +278,7 @@ __global__ void convn_same_kernel(float *output, float *data, float *kernel,
 } 
 
 __global__ void convn_full_kernel_with_shm(float *output, float *data,
-    float *kernel, int H, int W, int kH, int kW) { 
+    float *kernel, const int H, const int W, const int kH, const int kW) { 
 
   __CUDA_CONSTANTS__;
 
@@ -312,24 +295,21 @@ __global__ void convn_full_kernel_with_shm(float *output, float *data,
 
   // Copy data in global memory to shared memory
   float* D = K + kW * kH;
-  int WIDTH_STEP = blockDim.x + kW - 1,
-      HEIGHT_STEP = blockDim.y + kH - 1;
+  const int WIDTH_STEP = blockDim.x + kW - 1;
+  const int HEIGHT_STEP = blockDim.y + kH - 1;
+  const int nTotal = WIDTH_STEP * HEIGHT_STEP;
 
-  int nTotal = WIDTH_STEP * HEIGHT_STEP;
-  int avgToLoad  = nTotal / nThreads + 1;
+  x0 -= (kW - 1);
+  y0 -= (kH - 1);
 
-  for (int i=0; i<avgToLoad; ++i) {
-    int id = tid + i * nThreads;
-
-    if (id >= nTotal) break;
-
-    int xx = id / HEIGHT_STEP + x0 - (kW - 1),
-	yy = id % HEIGHT_STEP + y0 - (kH - 1);
+  for (; tid<nTotal; tid += nThreads) {
+    int xx = x0 + tid / HEIGHT_STEP,
+	yy = y0 + tid % HEIGHT_STEP;
 
     if (xx < 0 || xx >= W || yy < 0 || yy >= H)
-      D[id] = 0;
+      D[tid] = 0;
     else 
-      D[id] = data[ xx * H + yy ];
+      D[tid] = data[ xx * H + yy ];
   }
   __syncthreads();
   
@@ -337,9 +317,10 @@ __global__ void convn_full_kernel_with_shm(float *output, float *data,
     return;
 
   float sum = 0; 
+  D += tx * HEIGHT_STEP + ty;
   for (int i = 0; i < kW; ++i)
     for(int j = 0; j < kH; ++j)
-      sum += K[ i * kH + j ] * D[ (tx + i) * HEIGHT_STEP + (ty + j) ]; 
+      sum += K[ i * kH + j ] * D[ i * HEIGHT_STEP + j]; 
 
   output[ x * fH + y ] = sum;
 }
@@ -454,6 +435,7 @@ void ConvolutionalLayer::update_kernel(const mat& fin, const mat& delta) {
   grids.z = nOutputs;
 
   size_t SHM_SIZE = getSuitableShmConfig(grids, threads, imgOut.m, imgOut.n);
+  // printf("grids = (%lu, %lu, %lu), threads = (%lu, %lu, %lu) \n", grids.x, grids.y, grids.z, threads.x, threads.y, threads.z);
 
   for (size_t i=0; i<nInputs; ++i)
     for (size_t b=0; b<batch_size; ++b) {
@@ -627,51 +609,53 @@ vector<mat> de_concat(const mat& big, int N) {
   return smalls;
 }
 
-__global__ void downsample_kernel(float *dst, float *src, size_t scale, int H, int W) { 
-  int tx = threadIdx.x;
-  int ty = threadIdx.y;
-
+__global__ void downsample_kernel(float *dst, float *src, const uint8_t scale, const uint8_t H, const uint8_t W) { 
   // Matrix index
-  int x = blockIdx.x*blockDim.x + tx;
-  int y = blockIdx.y*blockDim.y + ty;
+  uint16_t x = blockIdx.x*blockDim.x + threadIdx.x;
+  uint16_t y = blockIdx.y*blockDim.y + threadIdx.y;
 
-  int h = H / scale,
-      w = W / scale;
-
-  src += blockIdx.z * H * W;
-  dst += blockIdx.z * h * w;
+  const uint8_t h = H / scale;
+  const uint8_t w = W / scale;
 
   if (y >= w || x >= h)
     return;
 
-  float sum;
-  for (int i=0; i<scale; ++i) {
-    for (int j=0; j<scale; ++j) {
-      if ( y*scale + i < W && x*scale + j < H )
-	sum += src[(y*scale + i) * H + (x*scale + j)];
+  dst += blockIdx.z * h * w + y * h + x;
+
+  y *= scale;
+  x *= scale;
+
+  float sum = 0;
+
+  src += blockIdx.z * H * W + y * H + x;
+
+  for (uint8_t i=0; i<scale; ++i) {
+    for (uint8_t j=0; j<scale; ++j) {
+      if ( y + i < W && x + j < H )
+	sum += src[i * H + j];
     }
   }
 
-  dst[y * h + x] = sum / (scale * scale);
+  *dst = sum / (scale * scale);
 }
 
-__global__ void upsample_kernel(float *dst, float *src, int h, int w, int H, int W) { 
-  int tx = threadIdx.x;
-  int ty = threadIdx.y;
+__global__ void upsample_kernel(float *dst, float *src, const uint8_t h, const uint8_t w, const uint8_t H, const uint8_t W) { 
 
   // Matrix index
-  int x = blockIdx.x*blockDim.x + tx;
-  int y = blockIdx.y*blockDim.y + ty;
+  const uint16_t x = blockIdx.x*blockDim.x + threadIdx.x;
+  const uint16_t y = blockIdx.y*blockDim.y + threadIdx.y;
 
   src += blockIdx.z * h * w;
   dst += blockIdx.z * H * W;
 
-  int scale = H / h;
+  uint8_t scale = H / h;
 
   if (y >= W || x >= H)
     return;
 
-  int sy = y / scale, sx = x / scale;
+  uint8_t sy = y / scale;
+  uint8_t sx = x / scale;
+
   if (sy == w) --sy;
   if (sx == h) --sx;
 
@@ -692,7 +676,7 @@ void SubSamplingLayer::feedForward(mat& fout, const mat& fin) {
     downsample_kernel<<<grids, threads>>>(
 	fout.getData() + i * fout.getRows(),
 	fin.getData() + i * fin.getRows(),
-	_scale, imgIn.m, imgIn.n);
+	(uint8_t) _scale, (uint8_t) imgIn.m, (uint8_t) imgIn.n);
   }
   CCE(cudaDeviceSynchronize());
 }
@@ -709,14 +693,14 @@ void SubSamplingLayer::feedBackward(mat& error, const mat& delta) {
 
   error.resize(imgIn.area() * getNumInputMaps() + 1, delta.getCols());
 
-  for (size_t i=0; i<delta.getCols(); ++i) {
-    // errors[i] = upsample(deltas[i], _input_img_size, get_output_img_size());
-
+  for (size_t i=0; i<delta.getCols(); ++i)
     upsample_kernel<<<grids, threads>>>(
 	error.getData() + i * error.getRows(),
 	delta.getData() + i * delta.getRows(),
 	imgOut.m, imgOut.n, imgIn.m, imgIn.n);
-  }
+
+  error *= 1 / (_scale * _scale);
+
   CCE(cudaDeviceSynchronize());
 }
 
@@ -739,7 +723,7 @@ mat downsample(const mat& x, size_t scale, SIZE s) {
   downsample_kernel<<<grids, threads>>>(
       output.getData(),
       x.getData(),
-      scale, H, W);
+      (uint8_t) scale, (uint8_t) H, (uint8_t) W);
 
   CCE(cudaDeviceSynchronize());
 
